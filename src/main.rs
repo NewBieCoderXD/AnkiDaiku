cffd322bec4a97ba9b902845a14f61947f812855
NewBieCoderXD <fruity5555@gmail.com>
2026-07-29 10:56:17 +0700

    feat: supports dist folder

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use clap::{Parser, Subcommand};
 enum Actions {
   Build {
     dir_path: String,
-    #[arg(short, long, default_value = "output.apkg")]
-    output: String,
+    #[arg(short, long)]
+    output: Option<String>,
     #[arg(short = 'C', long)]
     cards_dir: Option<String>,
     #[arg(long)]
@@ -28,7 +28,8 @@ fn main() {
 
   match &args.action {
     Actions::Build { dir_path, output, cards_dir, config } => {
-      if let Err(e) = actions::export::export_apkg(dir_path, output, cards_dir.as_deref(), config.as_deref()) {
+      let output = output.clone().unwrap_or_else(|| format!("{}/dist/output.apkg", dir_path));
+      if let Err(e) = actions::export::export_apkg(dir_path, &output, cards_dir.as_deref(), config.as_deref()) {
         eprintln!("Error: {}", e);
         std::process::exit(1);
       }
